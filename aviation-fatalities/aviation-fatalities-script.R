0df6bd328a50e1a619cd2d96c6d8c03ac6bbe963
Nicolas <nicolas.houlier@gmail.com>
2018-12-26 11:21:20 +0100

    exploration of data

--- aviation-fatalities/aviation-fatalities-script.R
+++ aviation-fatalities/aviation-fatalities-script.R
@@ -1,6 +1,20 @@
+############                         ############
+############   aviation-fatalities   ############
+############                         ############
+
+#Imports
 library(data.table)
 
+# Read from csv and display first 5 lines
 path_train <- getwd()
 path_train = paste(path_train, 'datasets/train.csv', sep='/')
 train_raw <- fread(file=path_train, header=TRUE, sep=",")
 head(train_raw, 5)
+
+# check for missing values
+sapply(train_raw, function(x) sum(is.na(x)))
+
+# print some basical statistics on this data.frame
+summary(train_raw)
+
+
