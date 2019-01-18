6085cf5ff825b296ff94061f32808932fa8d9aaa
Nicolas <nicolas.houlier@gmail.com>
2019-01-18 23:07:31 +0100

    update script

--- aviation-fatalities/aviation-fatalities-script.R
+++ aviation-fatalities/aviation-fatalities-script.R
@@ -17,6 +17,15 @@ sapply(train_raw, function(x) sum(is.na(x)))
 # print some basical statistics on this data.frame
 summary(train_raw)
 
+# list occurences of all values of "event" column
+table(unlist(train_raw$event))
+
+# convert categorical variable "event" into numerical variable
+train_raw$event <- as.numeric(factor(train_raw$event, levels=unique(train_raw$event)))
+head(train_raw$event)
+
 # Plots
 boxplot(split(train_raw$r, train_raw$event), main='Event by respiration')
+boxplot(split(train_raw$ecg, train_raw$event), main='Event by electrocardiogram')
+
 
